3891a84537d270c0f860f43676dba7b442354622
otsybulsky <helg37@gmail.com>
2018-03-25 16:32:23 +0300

    add BookDetail

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import BookList from '../containers/books_list'
+import BookDetail from '../containers/books_detail'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <BookList />
+        <BookDetail />
       </div>
     );
   }
--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,4 +1,5 @@
 export default function (local_state = null, action) {
+    
     switch (action.type) {
         case 'BOOK_SELECTED':
             return action.payload //to redux store 
