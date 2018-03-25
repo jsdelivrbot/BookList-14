9ecb629c680c8ba4e2b81d1a334bf909853e26e9
otsybulsky <helg37@gmail.com>
2018-03-25 15:52:45 +0300

    add ActiveBook reducer

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+export default function (local_state = null, action) {
+    switch (action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload //to redux store 
+    
+        default:
+            return local_state //to redux store
+    }
+}
